7fe0b220653efd2dab383d0195ba7a847ccf388e
Ahmar Suhail <ahmarsu@amazon.co.uk>
2025-08-14 14:35:13 +0100

    size

--- input-stream/src/main/java/software/amazon/s3/analyticsaccelerator/io/physical/prefetcher/SequentialReadProgression.java
+++ input-stream/src/main/java/software/amazon/s3/analyticsaccelerator/io/physical/prefetcher/SequentialReadProgression.java
@@ -48,6 +48,9 @@ public class SequentialReadProgression {
                 configuration.getSequentialPrefetchBase(),
                 Math.floor(configuration.getSequentialPrefetchSpeed() * generation));
 
+
+    System.out.println("PRINTING SIZE: " + size);
+
     return Math.min(128 * ONE_MB, size);
   }
 }
